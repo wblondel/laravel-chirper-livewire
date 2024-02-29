42d0ab383025ac4a1c4dc9e3153ece8857cae3a7
William Gérald Blondel <contact@williamblondel.fr>
2024-02-29 18:41:21 +0800

    Create Chirps

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,8 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
+Route::get('chirps', [\App\Http\Controllers\ChirpController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('chirps');
+
 require __DIR__.'/auth.php';
